eb1bf9c2c5989aac48c58f202829431eb88c925d
Hicham-Hal <halouanehicham@gmail.com>
2026-08-13 17:51:48 +0100

    adding checkgrammar fun

--- controllers/all.controller.js
+++ controllers/all.controller.js
@@ -18,16 +18,19 @@ export const getNotes = async(req, res) => {
 
         const data = await fs.promises.readFile(path.join('data', 'notes.json'), 'utf8')
         const parsedData = JSON.parse(data)
-        return res.render('notes', { notes: parsedData })
+        return res.status(200).json(parsedData)
     }catch(err){
         console.log(err)
+        return res.status(500).json({ msg: 'server error' })
     }
 }
 
 export const postNotes = async(req, res) => {
     const { title } = req.body
     let dataNotes = []
-
+    if(!req.file){
+        return res.status(401).json({ msg: 'You should implement the markdown file' })
+    }
     try{
         if(fs.existsSync(path.join('data', 'notes.json'))){
             const notes = await fs.promises.readFile(path.join('data', 'notes.json'), 'utf8')
@@ -43,9 +46,10 @@ export const postNotes = async(req, res) => {
         dataNotes.push(newNote)
 
         await fs.promises.writeFile(path.join('data',`notes.json`), JSON.stringify(dataNotes))
-        return res.status(200).json({ msg: "Note created" })
+        return res.status(201).json({ msg: "Note created" })
     }catch(err){
         console.log(err)
+        return res.status(500).json({ msg: 'server error' })
     }
 }
 
@@ -56,12 +60,16 @@ export const getSingleNote = async(req, res) => {
         if(!fs.existsSync(path.join('storage/notes', `${id}`))){
             return res.status(404).json({ msg: 'no note found' })
         }
+        const jsonData = await fs.promises.readFile(path.join('data', 'notes.json'), 'utf8')
+        const parsedJsonData = JSON.parse(jsonData)
+        const single = parsedJsonData.find(item => item.id === id)
         const data = await fs.promises.readFile(path.join('storage/notes', `${id}`), 'utf8')
         const rawHtml = marked(data)
         const returnedRaw = DOMPurify.sanitize(rawHtml)
-        return res.render('note', { note: returnedRaw })
+        return res.status(200).json({Note: single, HtmlRaw: returnedRaw})
     }catch(err){
         console.log(err)
+        return res.status(500).json({ msg: 'server error' })
     }
 }
 
@@ -76,17 +84,16 @@ export const deleteNote = async(req, res) => {
         const newData = parsedData.filter(item => item.id !== id)
         await fs.promises.unlink(path.join('storage/notes', `${id}`))
         await fs.promises.writeFile(path.join('data', 'notes.json'), JSON.stringify(newData))
-        return res.redirect('/notes')
+        return res.status(204).json()
     }catch(err){
         console.log(err)
+        return res.status(500).json({ msg: 'server error' })
     }
 }
 
 export const updateNote = async(req, res) => {
     const { id } = req.params
     const { title } = req.body
-    console.log(title)
-    const data = []
     try{
         if(!fs.existsSync(path.join('data', 'notes.json'))){
             return res.status(404).json({ msg: 'no note found' })
@@ -101,8 +108,30 @@ export const updateNote = async(req, res) => {
             await fs.promises.unlink(path.join('storage/notes', `${id}`))
         }
         await fs.promises.writeFile(path.join('data', 'notes.json'), JSON.stringify(parsedNotes))
-        res.json({ msg: 'ok' })
+        res.status(200).json(note)
     }catch(err){
         console.log(err)
+        return res.status(500).json({ msg: 'server error' })
+    }
+}
+
+export const checkGrammar = async(req, res) => {
+    const { id } = req.params
+    try{
+        const file = await fs.promises.readFile(path.join('storage/notes', `${id}`), 'utf8')
+        const params = new URLSearchParams({ text: file, language: 'en-US' })
+        const response = await fetch(`https://api.languagetool.org/v2/check`, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+            body: params
+        })
+        if(response.status !== 200){
+            return res.status(401).json({ msg: 'Something went wrong' })
+        }
+        const data = await response.json()
+        return res.status(200).json(data)
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({ msg: 'server error' })
     }
 }
--- routes/all.route.js
+++ routes/all.route.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { deleteNote, getNotes, getSingleNote, postNotes, updateNote } from '../controllers/all.controller.js'
+import { checkGrammar, deleteNote, getNotes, getSingleNote, postNotes, updateNote } from '../controllers/all.controller.js'
 import multer from 'multer'
 const route = express.Router()
 const upload = multer({ dest: 'storage/notes/' })
@@ -10,5 +10,6 @@ route.post('/', upload.single('markdown'), postNotes)
 route.get('/:id', getSingleNote)
 route.post('/delete/:id', deleteNote)
 route.put('/:id', upload.single('markdown'), updateNote)
+route.get('/grammar/:id', checkGrammar)
 
 export default route
